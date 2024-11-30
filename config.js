59ba4324e806847db163721390bfd8b29c5750a1
Mythwavie344 <Mythwavie@gmail.com>
2024-11-30 20:48:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.envSESSION_ID"DvYhERIK#icKlnDvwAonkNwpdhqtl8_fcpIgjSGnnlP5mF2exDsQ", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
